840c00e568871cb22ab380563ae0f47d6cc4b68b
Alex Reyes <reyes.alexanderreid@gmail.com>
2019-12-27 10:42:52 -0500

    Create basic front-end search structure
    Searchbox and search button added to app.jsx. Event handlers
    setup to watch the user's search queries dynamically.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 
-const App = () => (
-    <div>Foobar</div>
-);
+const App = () => {
+
+    const [searchText, setSearchText] = useState("");
+    const updateSearchText = e => setSearchText(e.target.value);
+    const sendSearchRequest = () => alert(searchText);
+
+    return (
+        <Fragment>
+            <h2>Search box</h2>
+            <input
+                type="text" 
+                placeholder="Search..."
+                onChange={updateSearchText}
+            />
+            <button onClick={sendSearchRequest}>Search</button>
+            <div className="search-results"></div>
+        </Fragment>
+    );
+
+};
 
 export default App;
