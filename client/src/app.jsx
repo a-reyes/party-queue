d0e98d37aecd99425fad9188dd7b16a9fa5c027c
Alex Reyes <reyes.alexanderreid@gmail.com>
2019-12-27 13:08:31 -0500

    Add track result rendering from sample API data

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,10 +1,29 @@
 import React, { Fragment, useState } from "react";
 
+// TEMP
+import sampleResponse from "./sampleresponse.json";
+
+const TrackListItem = ({ trackInfo }) => (
+    <div className="search-result">
+        <img src={trackInfo.album.images[2].url} />
+        <a href={trackInfo.external_urls.spotify}>
+            {trackInfo.name} - {trackInfo.artists[0].name} {trackInfo.explicit ? "[Explicit]" : ""}
+        </a>
+    </div>
+);
+
 const App = () => {
 
+    // Search text entered into the text input
     const [searchText, setSearchText] = useState("");
     const updateSearchText = e => setSearchText(e.target.value);
-    const sendSearchRequest = () => alert(searchText);
+
+    // Array of results recieved from the server
+    const [searchResults, setSearchResults] = useState([]);
+    const sendSearchRequest = () => {
+        const trackInfo = sampleResponse.tracks.items[0];
+        setSearchResults(searchResults.concat(sampleResponse.tracks.items));
+    };
 
     return (
         <Fragment>
@@ -15,7 +34,11 @@ const App = () => {
                 onChange={updateSearchText}
             />
             <button onClick={sendSearchRequest}>Search</button>
-            <div className="search-results"></div>
+            <div className="search-results">{
+                searchResults.map(trackInfo => (
+                    <TrackListItem key={trackInfo.id} trackInfo={trackInfo} />
+                ))
+            }</div>
         </Fragment>
     );
 
