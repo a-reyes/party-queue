084bef75a99c5827805ad2c377e2ac2cbe5c4981
Alex Reyes <reyes.alexanderreid@gmail.com>
2020-01-01 16:01:01 -0500

    Add basic functionality to PlaybackControls component
    Buttons allowing AJAX requests to the 4 /playback routes created
    and implemented.

--- client/src/components/playback-controls/playback-controls.jsx
+++ client/src/components/playback-controls/playback-controls.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-const PlaybackControls = props => (
-    <div>Controls</div>
-);
+// TODO: Change all routes to return JSON objects.
+
+const playPrevious = () => {
+    fetch("/playback/previous")
+    .then(res => console.log(res));
+};
+
+const playNext = () => {
+    fetch("/playback/next")
+    .then(res => console.log(res));
+}
+
+const PlaybackControls = props => {
+    // TODO: Use server requests to verify
+    const [isPlaying, setIsPlaying] = useState(true);
+    const playPause = () => {
+        let reqRoute;
+        if (isPlaying) {
+            reqRoute = "/playback/pause";
+        } else {
+            reqRoute = "/playback/resume";
+        }
+
+        fetch(reqRoute)
+        .then(res => {
+            if (res.status === 200) {
+                setIsPlaying(!isPlaying);
+            } else {
+                alert("An error occurred.");
+                console.log(res);
+            }
+        });
+    }
+
+    return (
+        <div>
+            <button onClick={playPrevious}>Previous</button>
+            <button onClick={playPause}>
+                {isPlaying ? "Pause" : "Play"}
+            </button>
+            <button onClick={playNext}>Next</button>
+        </div>
+    );
+};
 
 export default PlaybackControls;
