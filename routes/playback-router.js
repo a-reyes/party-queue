d88887cbea4ad902c0d8dc14c07c15d7c0a47c0c
Alex Reyes <reyes.alexanderreid@gmail.com>
2020-01-01 14:40:08 -0500

    Implement playing previous song route
    GET requests to /playback/previous will now play the last song
    the user listened to. As currently implemented, this route will
    always play the previous song, and not skip to the beginning of the
    current track regardless of the duration that has been played.

--- routes/playback-router.js
+++ routes/playback-router.js
@@ -45,8 +45,36 @@ router.get("/next", async (req, res) => {
     }
 });
 
-router.get("/previous", (req, res) => {
-    
+router.get("/previous", async (req, res) => {
+    const reqOptions = {
+        uri: "https://api.spotify.com/v1/me/player/previous",
+        headers: {
+            Authorization: `Bearer ${req.session.accessToken}`
+        },
+        resolveWithFullResponse: true
+    };
+
+    let response;
+    try {
+        response = await request.post(reqOptions);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error 500: an error occurred");
+        return;
+    }
+
+    const body = response.body;
+    switch (response.statusCode) {
+        case 204:
+            console.log("Previous song played successfully...");
+            res.status(200).send("Success");
+            break;
+        default:
+            // TODO: Implement 404 (no devices) or 403 (not premium)
+            console.log(response.statusCode);
+            console.log(body);
+            res.json(body);
+    }
 });
 
 module.exports = router;
