95b8f2076b4e138f9b9fd94296368d47b4699395
Alex Reyes <reyes.alexanderreid@gmail.com>
2020-01-02 15:12:39 -0500

    Add socket.io events to handle song pause/resume
    Logic moved from loginRouter to socket.io emit events. Song
    playback can now be fully controlled via socket.io.

--- server.js
+++ server.js
@@ -228,6 +228,78 @@ io.on("connect", socket => {
         }
     });
 
+    socket.on("pause-playback", async () => {
+        // TODO: Check if song already paused
+        console.log("Pausing playback...");
+
+        const userSession = socket.handshake.session;
+        const reqOptions = {
+            uri: "https://api.spotify.com/v1/me/player/pause",
+            headers: {
+                Authorization: `Bearer ${userSession.accessToken}`
+            },
+            resolveWithFullResponse: true
+        };
+
+        try {
+            response = await request.put(reqOptions);
+        } catch (err) {
+            console.log(err);
+            socket.emit("server-error", {
+                msg: err.toString() // TODO: Remove
+            });
+            return;
+        }
+
+        const body = response.body;
+        switch (response.statusCode) {
+            case 204:
+                console.log("Song paused successfully...");
+                // Send a success message to client?
+                break;
+            default:
+                // TODO: Implement 404 (no devices) or 403 (not premium)
+                console.log(response.statusCode);
+                console.log(body);
+        }
+    });
+
+    socket.on("resume-playback", async () => {
+        // TODO: Check if song already playing
+        console.log("Resuming playback...");
+
+        const userSession = socket.handshake.session;
+        const reqOptions = {
+            uri: "https://api.spotify.com/v1/me/player/play",
+            headers: {
+                Authorization: `Bearer ${userSession.accessToken}`
+            },
+            resolveWithFullResponse: true
+        };
+
+        try {
+            response = await request.put(reqOptions);
+        } catch (err) {
+            console.log(err);
+            socket.emit("server-error", {
+                msg: err.toString() // TODO: Remove
+            });
+            return;
+        }
+
+        const body = response.body;
+        switch (response.statusCode) {
+            case 204:
+                console.log("Song resumed successfully...");
+                updateSong(socket);
+                break;
+            default:
+                // TODO: Implement 404 (no devices) or 403 (not premium)
+                console.log(response.statusCode);
+                console.log(body);
+        }
+    });
+
     socket.on("disconnect", () => {
         console.log("A user disconnected.");
     });
